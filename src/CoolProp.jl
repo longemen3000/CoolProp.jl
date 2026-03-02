bbd89704a493a3ce021cce08548e598bb3115a87
Andrés Riedemann <38795484+longemen3000@users.noreply.github.com>
2026-03-01 23:59:57 -0300

    _get_unit: change default from true to nothing

--- src/CoolProp.jl
+++ src/CoolProp.jl
@@ -88,15 +88,15 @@ function _get_unit(param::AbstractString, is_ha::Bool, val::Real)
     return _get_unit(param,is_ha,nothing)
 end
 
-_get_unit(param::AbstractString, is_ha::Bool, ::Nothing) = true
+_get_unit(param::AbstractString, is_ha::Bool, ::Nothing) = nothing
 _si_value(::Bool,value) = value
 
 function PropsSI(output::AbstractString, name1::AbstractString, value1::Number, name2::AbstractString, value2::Number, fluid::AbstractString)
     unit1 = _get_unit(name1,false,value1)
     unit2 = _get_unit(name2,false,value2)
     out = PropsSI(output, name1, _si_value(unit1,value1), name2, _si_value(unit2,value2), fluid)
-    unit1 == true && unit2 == true && (return out)
-    unit1 != true && (return out*_get_unit(output,false,value1))
+    unit1 === nothing && unit2 === nothing && (return out)
+    unit1 !== nothing && (return out*_get_unit(output,false,value1))
     return out*_get_unit(output,false,value2)
 end
 
@@ -677,9 +677,9 @@ function HAPropsSI(output::AbstractString, name1::AbstractString, value1::Number
     unit2 = _get_unit(name2,true,value2)
     unit3 = _get_unit(name3,true,value3)
     out = HAPropsSI(output, name1, _si_value(unit1,value1), name2, _si_value(unit2,value2), name3, _si_value(unit3,value3))
-    unit1 == true && unit2 == true && unit3 == true && (return out)
-    unit1 != true && (return out*_get_unit(output,true,value1))
-    unit2 != true && (return out*_get_unit(output,true,value2))
+    unit1 === nothing && unit2 === nothing && unit3 === nothing && (return out)
+    unit1 !== nothing && (return out*_get_unit(output,true,value1))
+    unit2 !== nothing && (return out*_get_unit(output,true,value2))
     return out*_get_unit(output,true,value3)
 end
 
