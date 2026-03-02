def97de048a1e4082b924a84c2388ab09aedad56
Andrés Riedemann <38795484+longemen3000@users.noreply.github.com>
2026-03-01 23:34:24 -0300

    typo

--- src/CoolProp.jl
+++ src/CoolProp.jl
@@ -97,7 +97,7 @@ function PropsSI(output::AbstractString, name1::AbstractString, value1::Number, 
     out = PropsSI(output, name1, _si_value(unit1,value1), name2, _si_value(unit2,value2), fluid)
     unit1 == true && unit2 == true && (return out)
     out1 != true && (return out*_get_unit(output,false,value1))
-    return out*_get_unit(output,falsevalue2)
+    return out*_get_unit(output,false,value2)
 end
 
 """
