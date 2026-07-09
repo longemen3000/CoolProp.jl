023c522d109e3bd80e88b3b5ced7c3b6482b2d53
Andrés Riedemann <38795484+longemen3000@users.noreply.github.com>
2026-07-08 20:55:23 -0400

    try with export_prefs = true

--- test/CoolPropDevLoader/src/CoolPropDevLoader.jl
+++ test/CoolPropDevLoader/src/CoolPropDevLoader.jl
@@ -100,12 +100,12 @@ function use_dev_library(::CompileCoolPropFromSourceforge)
 end
 
 function use_dev_library(lib_src::String)
-    set_preferences!((COOLPROP_UUID,"CoolProp"),"coolprop_library" => lib_src, force = true,active_project_only = false)
+    set_preferences!((COOLPROP_UUID,"CoolProp"),"coolprop_library" => lib_src, force = true,active_project_only = false,export_prefs = true)
     @info "Preference set to $lib_src"
 end
 
 function use_default_library()
-    delete_preferences!((COOLPROP_UUID,"CoolProp"),"coolprop_library",force = true,active_project_only = false)
+    delete_preferences!((COOLPROP_UUID,"CoolProp"),"coolprop_library",force = true,active_project_only = false,export_prefs = true)
 end
 
 end #module
